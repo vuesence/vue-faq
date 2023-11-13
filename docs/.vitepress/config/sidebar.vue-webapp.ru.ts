188f00a8942531f567f000c6a7cac8f75f6b61e2
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-11-13 12:09:52 +0700

    feat: vue-webapp navigation

---
+++ docs/.vitepress/config/sidebar.vue-webapp.ru.ts
@@ -0,0 +1,5 @@
+export default [
+  { text: "О приложении", link: "/vue-webapp/ru/" },
+  { text: "Цели", link: "/vue-webapp/ru/summary" },
+  { text: "Идеология", link: "/vue-webapp/ru/ideology" },  
+];
