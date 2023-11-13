4ad7e46a560a61a549ac740873a2725bafd6ec88
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-11-13 18:57:44 +0700

    chore: add  sidebar

--- docs/.vitepress/config/sidebar.vue-webapp.ru.ts
+++ docs/.vitepress/config/sidebar.vue-webapp.ru.ts
@@ -1,4 +1,4 @@
-export default [
+  export default [
   { text: "О приложении", link: "/vue-webapp/" },
   { text: "Цели", link: "/vue-webapp/objectives" },
   { text: "Принципы", link: "/vue-webapp/guidelines" },  
