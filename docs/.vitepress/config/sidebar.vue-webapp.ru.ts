080b5541960aba9c40b82e1f32bc526fe477d668
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-11-14 13:48:19 +0700

    chore: add 'getting started'

--- docs/.vitepress/config/sidebar.vue-webapp.ru.ts
+++ docs/.vitepress/config/sidebar.vue-webapp.ru.ts
@@ -2,4 +2,5 @@ export default [
   { text: "О приложении", link: "/vue-webapp/" },
   { text: "Цели", link: "/vue-webapp/goals" },
   { text: "Идеология", link: "/vue-webapp/ideology" },  
+  { text: "Установка", link: "/vue-webapp/getting-started" },  
 ];
