0c4a975b9edf682375f9f3d5b87f5df583f7fc4a
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-11-15 20:40:14 +0700

    feat: correct i18n redirect

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -29,7 +29,7 @@ export default defineConfig({
           `const init = localStorage.getItem("vue-faq-init") ?? false;
           localStorage.setItem("vue-faq-init", true);
           const isGoogleBot = window.navigator.userAgent.includes('googlebot');
-          // if (navigator.language != "ru-RU" && !init && !isGoogleBot) document.location="/en" + window.location.pathname;
+          /* if (navigator.language != "ru-RU" && !init && !isGoogleBot) document.location="/en" + window.location.pathname; */
           if (navigator.language != "ru-RU" && !init && !isGoogleBot) document.location="/en/";
           `,
         ],
