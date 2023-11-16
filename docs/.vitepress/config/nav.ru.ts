72d927ebe245e7e487461109d7aaa498ff7e99ec
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-11-16 10:02:30 +0700

    chore: removed links

--- docs/.vitepress/config/nav.ru.ts
+++ docs/.vitepress/config/nav.ru.ts
@@ -2,7 +2,7 @@ export default [
   { text: "Главная", link: "/" },
   { text: "FAQ", link: "/misc/introduction" },
   { text: "Учебник", link: "/book/ru/" },
-  { text: "vue-webapp", link: "/vue-webapp/" },
+  // { text: "vue-webapp", link: "/vue-webapp/" },
   {
     text: "v 1.3",
     items: [
