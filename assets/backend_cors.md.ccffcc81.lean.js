543d6306e83130d7e7910b89948fe007ccd53c82
altrusl <ruslan.makarov@gmail.com>
2023-11-16 10:43:54 +0000

    Deploying to gh-pages from @ vuesence/vue-faq@dd80d5716526df5df57daf6ff66913c42f33887c 🚀

---
+++ assets/backend_cors.md.ccffcc81.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as s,c as a,Q as t}from"./chunks/framework.254bf437.js";const u=JSON.parse('{"title":"CORS","description":"","frontmatter":{},"headers":[],"relativePath":"backend/cors.md","filePath":"backend/cors.md"}'),o={name:"backend/cors.md"},c=t("",4),l=[c];function n(r,p,i,d,h,g){return s(),a("div",null,l)}const v=e(o,[["render",n]]);export{u as __pageData,v as default};
