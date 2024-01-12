3c8b457b3b19f0f3d284523e4e3c27d19456b7e9
altrusl <ruslan.makarov@gmail.com>
2024-01-12 12:05:54 +0000

    Deploying to gh-pages from @ vuesence/vue-faq@dbfa7547c89c962b5f0ee6d7433b2941c7ebe711 🚀

--- assets/en_book_index.md.e5Ibh02C.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,c as t,o}from"./chunks/framework.lBFuN894.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"en/book/index.md","filePath":"en/book/index.md","lastUpdated":1705050017000}'),a={name:"en/book/index.md"};function n(r,s,d,c,i,p){return o(),t("div")}const f=e(a,[["render",n]]);export{m as __pageData,f as default};
